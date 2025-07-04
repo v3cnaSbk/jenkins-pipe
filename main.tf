9cf719b4d4d73b4ab98f72245dbeeb1c6b8b57cd
Dhiraj <elixir@DHIRAJ-KUMAR.local>
2025-07-04 16:09:39 +0530

    main

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "web_instance" {
+  source = "./modules"
+  ami = var.ami
+  instance_type = var.instance_type
+  instance_name = var.instance_name
+  instance = var.instance_name
+  instance = var.instance_name
+}
+
