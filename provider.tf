879b677d3a0c9dc14c8fd50a2f76e80b55bfa9e4
v3cnaSbk <110019539+v3cnaSbk@users.noreply.github.com>
2025-07-07 22:37:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIATCKATA5I5G6BYPG5"
-  secret_key =  "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"
+  access_key = ""
+  secret_key =  ""
 }
